7b72ca5cbb3af4a641f0ef5aecbe918e3589f2c2
Krisztian Nagy <k.nagy.k98@gmail.com>
2021-05-02 00:52:59 +0100

    mobile tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,11 +6,14 @@ export default class MyDocument extends Document {
             <Html lang="en">
                 <Head>
                     <meta charSet="utf-8" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta name="HandheldFriendly" content="true" />
                     <meta name="description" content="Covid-19 Travel App to get prepared before the move." />
                     <meta name="keywords" content="covid, virus, 19, covid-19, pandemic, form, travel, links, information" />
                     <meta name="og:title" content="C19 | Travel App" />
                     <meta name="og:description" content="C19 Travel App to get prepared before the move." />
                     <meta name="og:image" content="/icons/icon_192x192.png" />
+                    <meta name="og:url" content="https://c19travel.herokuapp.com/" />
                     <meta name="theme-color" content="#F3F3F4" />
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="icon" href="/icons/icon_192x192.png" />
@@ -25,4 +28,3 @@ export default class MyDocument extends Document {
     }
 }
 
-//<meta name="og:url" content="https://chrys-code.github.io/portfolio/" />
